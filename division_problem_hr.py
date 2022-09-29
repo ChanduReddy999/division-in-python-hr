e8d5ac10d1617e285d662803ced48e5f298fc798
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-09-29 23:29:22 +0530

    Add files via upload

---
+++ division_problem_hr.py
@@ -0,0 +1,20 @@
+"""
+Division of two numbers in python-hackerrank
+"""
+
+"""Method 1"""
+
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+#     print(a//b)
+#     print(a/b)
+
+
+"""Method 2"""
+
+# a=21
+# b=3
+# print(a//b)
+# print(a/b)
+
